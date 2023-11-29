fdfbfe3ff1c46d58a18ab1a8b41d4ad21ad55b52
Fred-Abongo <ngoloabongo32@gmail.com>
2023-11-28 19:00:31 -0800

    update all changes

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    string = ""
+    for char in str:
+        if char >= 'a' and char <= 'z':
+            string += chr((ord(char) - 32))
+        else:
+            string += char
+    print("{}".format(string))
