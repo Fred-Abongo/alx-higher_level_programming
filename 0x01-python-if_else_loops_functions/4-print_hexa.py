d25dfbcb94fd6cab5f31e3bfe782f69e04ae45d0
Fred-Abongo <ngoloabongo32@gmail.com>
2023-11-29 18:46:45 -0800

    update all changes in 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-        print("{} = 0x{:x}".format(i, i))
+for x in range(0, 100):
+    if x == 99:
+        print("{}".format(x))
+    else:
+        print("{:02}".format(x), end=", ")
